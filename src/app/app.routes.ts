e1f6f497476f22f13f9dc901ffd7c562674c5a0d
georgigsimeonov <georgi.simeonov.jr@gmail.com>
2025-04-09 00:53:48 +0300

    Updated basic structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { PageNotFoundComponent } from './components/other/page-not-found/page-not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'form', pathMatch: 'full' },
+  {
+    path: 'form',
+    loadComponent: () =>
+      import('./components/form/form.component').then(
+        (mod) => mod.FormComponent,
+      ),
+    title: 'Form Builder',
+  },
+  { path: '**', component: PageNotFoundComponent },
+];
---
+++ src/app/components/form/form-structure-input/form-structure-input.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form-structure-input',
+  imports: [],
+  templateUrl: './form-structure-input.component.html',
+  styleUrl: './form-structure-input.component.scss'
+})
+export class FormStructureInputComponent {
+
+}
