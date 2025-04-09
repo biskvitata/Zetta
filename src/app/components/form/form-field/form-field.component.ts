4df39af4fb14f31a4109c055a069fc3a02006ebf
georgigsimeonov <georgi.simeonov.jr@gmail.com>
2025-04-09 18:11:04 +0300

    Added support for dropdown

--- src/app/components/form/form-field/form-field.component.ts
+++ src/app/components/form/form-field/form-field.component.ts
@@ -4,10 +4,11 @@ import { FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatRadioModule } from '@angular/material/radio';
+import { MatSelectModule } from '@angular/material/select';
 
 @Component({
   selector: 'app-form-field',
-  imports: [CommonModule, FormsModule, ReactiveFormsModule, MatFormFieldModule, MatInputModule, MatRadioModule],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule, MatFormFieldModule, MatInputModule, MatRadioModule, MatSelectModule],
   templateUrl: './form-field.component.html',
   styleUrl: './form-field.component.scss'
 })
