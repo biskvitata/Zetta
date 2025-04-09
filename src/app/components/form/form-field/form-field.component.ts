e1225da0e154b3114c910400362e0284c35007a8
georgigsimeonov <georgi.simeonov.jr@gmail.com>
2025-04-09 18:03:37 +0300

    Added support for radio buttons

--- src/app/components/form/form-field/form-field.component.ts
+++ src/app/components/form/form-field/form-field.component.ts
@@ -3,10 +3,11 @@ import { Component, Input } from '@angular/core';
 import { FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { MatRadioModule } from '@angular/material/radio';
 
 @Component({
   selector: 'app-form-field',
-  imports: [CommonModule, FormsModule, ReactiveFormsModule, MatFormFieldModule, MatInputModule],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule, MatFormFieldModule, MatInputModule, MatRadioModule],
   templateUrl: './form-field.component.html',
   styleUrl: './form-field.component.scss'
 })
