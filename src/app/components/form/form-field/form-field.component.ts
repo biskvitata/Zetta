5079e9dd7dc4203787a784b744aad49d3539b07b
georgigsimeonov <georgi.simeonov.jr@gmail.com>
2025-04-09 17:39:36 +0300

    Forms fields customization added for text input

--- src/app/components/form/form-field/form-field.component.ts
+++ src/app/components/form/form-field/form-field.component.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { FormStructureInputComponent } from '../form-structure-input/form-structure-input.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-form-field',
-  imports: [CommonModule, FormsModule, ReactiveFormsModule],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule, MatFormFieldModule, MatInputModule],
   templateUrl: './form-field.component.html',
   styleUrl: './form-field.component.scss'
 })
