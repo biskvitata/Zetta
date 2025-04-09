25cd3df060939c0db445940df1cb3089b8fbb2a6
georgigsimeonov <georgi.simeonov.jr@gmail.com>
2025-04-09 22:45:24 +0300

    Added support for checkbox

--- src/app/components/form/form-field/form-field.component.ts
+++ src/app/components/form/form-field/form-field.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatRadioModule } from '@angular/material/radio';
@@ -8,7 +9,7 @@ import { MatSelectModule } from '@angular/material/select';
 
 @Component({
   selector: 'app-form-field',
-  imports: [CommonModule, FormsModule, ReactiveFormsModule, MatFormFieldModule, MatInputModule, MatRadioModule, MatSelectModule],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule, MatFormFieldModule, MatInputModule, MatRadioModule, MatSelectModule, MatCheckboxModule],
   templateUrl: './form-field.component.html',
   styleUrl: './form-field.component.scss'
 })
