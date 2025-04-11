7fcbf25f221ff3b5bbaf49744ec21741abc8a0bc
georgigsimeonov <georgi.simeonov.jr@gmail.com>
2025-04-11 03:57:07 +0300

    Initial commit with unit tests

--- src/app/components/form/form-dropdown/form-dropdown.component.spec.ts
+++ src/app/components/form/form-dropdown/form-dropdown.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { FormDropdownComponent } from './form-dropdown.component';
+import { FormControl } from '@angular/forms';
+import { Field } from '../../../models/formStructure.interface';
 
 describe('FormDropdownComponent', () => {
   let component: FormDropdownComponent;
@@ -14,6 +16,9 @@ describe('FormDropdownComponent', () => {
 
     fixture = TestBed.createComponent(FormDropdownComponent);
     component = fixture.componentInstance;
+
+    component.control = new FormControl('');
+    component.field = { label: 'Test Dropdown Label', key: "Test Dropdown Key", type: "Test Dropdown Type", placeholder: "Test Dropdown Placeholder" } as Field;
     fixture.detectChanges();
   });
 
